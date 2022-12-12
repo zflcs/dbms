7a9e02ba0379d802d71dd30db70f72c4516f75e5
HaoLiu97 <519555327@qq.com>
2022-12-12 20:21:39 +0800

    add get file names api

--- dbms/src/main/java/dbms/Controller/HdfsController.java
+++ dbms/src/main/java/dbms/Controller/HdfsController.java
@@ -1,12 +1,20 @@
 package dbms.Controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -21,6 +29,8 @@ public class HdfsController {
     public static Configuration CONF;
     public static FileSystem FS;
 
+    final private Gson gson = new Gson();
+
     static {
         HdfsController.UGI = UserGroupInformation.createRemoteUser("hadoop");
         HdfsController.CONF = new Configuration();
@@ -34,10 +44,25 @@ public class HdfsController {
     }
 
     @CrossOrigin
+    @RequestMapping(value = "/all_file_name")
+    String get_all_file_names(@RequestParam(value="article_id") int article_id, HttpServletResponse response) {
+        Path path = new Path("/user/hadoop/articles/article" + article_id);
+        ArrayList<String> paths = new ArrayList<>();
+        try {
+            FileStatus[] stats = FS.listStatus(path);
+            for (FileStatus stat : stats) {
+                paths.add(stat.getPath().getName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return gson.toJson(paths);
+    }
 
-    @RequestMapping(value = "/txt_content")
-    void text(@RequestParam(value="article_id") int article_id, @RequestParam(value="txt_name") String txt_name, HttpServletResponse response) {
-        Path text = new Path("/user/hadoop/articles/article" + article_id + "/" + txt_name);
+    @CrossOrigin
+    @RequestMapping(value = "/file_content")
+    void get_file(@RequestParam(value="filepath") String filepath, HttpServletResponse response) {
+        Path text = new Path(filepath);
         try {
             FSDataInputStream ins = FS.open(text);
             // ByteBuffer bf = new ByteBuffer();
@@ -53,46 +78,4 @@ public class HdfsController {
         }
         // return "ok";
     }
-
-    @CrossOrigin
-    @RequestMapping(value = "/img_content")
-    void img(@RequestParam(value="article_id") int article_id, @RequestParam(value="img_name") String img_name, HttpServletResponse response) {
-        Path img_path = new Path("/user/hadoop/articles/article" + article_id + "/" + img_name);
-        try {
-            FSDataInputStream ins = FS.open(img_path);
-            // ByteBuffer bf = new ByteBuffer();
-            // ins.read(bf);
-            int ch = ins.read();
-            while (ch != -1) {
-                response.getOutputStream().write(ch);
-                ch = ins.read();
-            }
-            response.getOutputStream().flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // return "ok";
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/video_content")
-    void video(@RequestParam(value="article_id") int article_id, @RequestParam(value="video_name") String video_name, HttpServletResponse response) {
-        Path video_path = new Path("/user/hadoop/articles/article" + article_id + "/" + video_name);
-        System.out.println(video_path.toUri().toString());
-        try {
-            FSDataInputStream ins = FS.open(video_path);
-            // ByteBuffer bf = new ByteBuffer();
-            // ins.read(bf);
-            int ch = ins.read();
-            while (ch != -1) {
-                response.getOutputStream().write(ch);
-                ch = ins.read();
-            }
-            response.getOutputStream().flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // return "ok";
-    }
-
 }
